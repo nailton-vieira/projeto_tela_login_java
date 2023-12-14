dc060070107fc9191da2192696760f6e60c7addd
nailton-vieira <nailtonvieira@gmail.com>
2023-12-14 15:54:58 -0300

    Criando a classe Telalogin

---
+++ src/Telalogin.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+
+public class Telalogin extends JFrame{
+
+    private JPanel contentPane;
+    private JTextField txtusuario;
+    private JPasswordField passsenha;
+
+public static void main(String[] args) {
+    
+}
+
+    
+}
